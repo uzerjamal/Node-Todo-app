c0293da91c80dbb274ace49ef29daf77dddc627e
Uzer Jamal <uzerjamal@gmail.com>
2021-01-19 15:30:08 +0530

    Added UI

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,11 @@
 const { render } = require("ejs");
 
+let data = ['Get milk', 'Walk dog', 'Do coding', 'Go Home'];
+
 module.exports = (app) => {
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {todos: data});
 });    
 
 app.post('/', (req, res) =>{
