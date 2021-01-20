ab396daaafa96e5bface823d60655255121110b9
Uzer Jamal <uzerjamal@gmail.com>
2021-01-20 10:56:39 +0530

    Added port for heroku

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const todoController = require('./controllers/todoController');
 const app = express();
-const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
 todoController(app);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on port ${port}`);
 });
